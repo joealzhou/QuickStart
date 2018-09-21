c7ddb000eb48c35a86e6387cc334358c9df18064
周强 <joealzhou@sina.com>
2018-09-21 17:47:37 +0800

    project init

---
+++ QuickStart/QuickStart.h
@@ -0,0 +1,12 @@
+//
+//  QuickStart.h
+//  QuickStart
+//
+//  Created by zhu on 16/9/17.
+//  Copyright © 2016年 zxp. All rights reserved.
+//
+
+#import <stdio.h>
+#import <stdlib.h>
+#import <objc/runtime.h>
+
